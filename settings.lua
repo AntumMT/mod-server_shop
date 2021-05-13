e62f33e2839cfb0e78e0742fb45235a06b1051f2
Jordan Irwin <antumdeluge@gmail.com>
2021-05-12 22:15:27 -0700

    Enable setting currency defaults by default

--- settings.lua
+++ settings.lua
@@ -8,5 +8,5 @@
 --
 --  @setting server_shop.use_currency_defaults
 --  @settype bool
---  @default false
-server_shop.use_currency_defaults = core.settings:get_bool("server_shop.use_currency_defaults", false)
+--  @default true
+server_shop.use_currency_defaults = core.settings:get_bool("server_shop.use_currency_defaults", true)
