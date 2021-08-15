50b833a10d6766519dada86d2fcc930ce23e9a97
Jordan Irwin <antumdeluge@gmail.com>
2021-08-15 16:41:31 -0700

    Add "tall" shop nodes

--- node.lua
+++ node.lua
@@ -3,90 +3,87 @@ local ss = server_shop
 local S = core.get_translator(ss.modname)
 
 
-core.register_node(ss.modname .. ":sell", {
-	description = S("Seller Shop"),
-	--drawtype = "nodebox",
-	drawtype = "normal",
-	tiles = {
-		"server_shop_side.png",
-		"server_shop_side.png",
-		"server_shop_side.png",
-		"server_shop_side.png",
-		"server_shop_side.png",
-		"server_shop_front.png",
-		"server_shop_side.png",
+local def = {
+	base = {
+		groups = {oddly_breakable_by_hand=1,},
+		paramtype2 = "facedir",
+		after_place_node = function(pos, placer)
+			-- set node owner
+			core.get_meta(pos):set_string("owner", placer:get_player_name())
+		end,
+		can_dig = function(pos, player)
+			return ss.is_shop_owner(pos, player) or ss.is_shop_admin(player)
+		end,
 	},
-	--[[
-	drawtype = "mesh",
-	mesh = "server_shop.obj",
-	tiles = {"server_shop_mesh.png",},
-	selection_box = {
-		type = "fixed",
-		fixed = {-0.5, -0.5, -0.5, 0.5, 1.5, 0.5},
+	short = {
+		drawtype = "normal",
+		tiles = {
+			"server_shop_side.png",
+			"server_shop_side.png",
+			"server_shop_side.png",
+			"server_shop_side.png",
+			"server_shop_side.png",
+			"server_shop_front.png",
+			"server_shop_side.png",
+		},
 	},
-	collision_box = {
-		type = "fixed",
-		fixed = {-0.5, -0.5, -0.5, 0.5, 1.45, 0.5},
+	tall = {
+		drawtype = "mesh",
+		mesh = "server_shop.obj",
+		tiles = {"server_shop_mesh.png",},
+		selection_box = {
+			type = "fixed",
+			fixed = {-0.5, -0.5, -0.5, 0.5, 1.5, 0.5},
+		},
+		collision_box = {
+			type = "fixed",
+			fixed = {-0.5, -0.5, -0.5, 0.5, 1.5, 0.5},
+		},
 	},
-	node_box = {
-		type = "fixed",
-		fixed = {-0.5, -0.5, -0.5, 0.5, 1.5, 0.5},
+	sell = {
+		description = S("Seller Shop"),
+		on_rightclick = function(pos, node, player, itemstack, pointed_thing)
+			local pmeta = player:get_meta()
+
+			-- store node pos in player meta for retrieval in callbacks
+			pmeta:set_string(ss.modname .. ":pos", core.serialize(pos))
+			-- store selected index in player meta for retrieval in callbacks
+			pmeta:set_int(ss.modname .. ":selected", 1)
+
+			ss.show_formspec(pos, player)
+		end,
 	},
-	]]
-	groups = {oddly_breakable_by_hand=1,},
-	paramtype2 = "facedir",
-	after_place_node = function(pos, placer)
-		-- set node owner
-		core.get_meta(pos):set_string("owner", placer:get_player_name())
-	end,
-	on_rightclick = function(pos, node, player, itemstack, pointed_thing)
-		local pmeta = player:get_meta()
+	buy = {
+		description = S("Buyer Shop"),
+		on_rightclick = function(pos, node, player, itemstack, pointed_thing)
+			local pmeta = player:get_meta()
 
-		-- store node pos in player meta for retrieval in callbacks
-		pmeta:set_string(ss.modname .. ":pos", core.serialize(pos))
-		-- store selected index in player meta for retrieval in callbacks
-		pmeta:set_int(ss.modname .. ":selected", 1)
+			-- store node pos in player meta for retrieval in callbacks
+			pmeta:set_string(ss.modname .. ":pos", core.serialize(pos))
+			-- store selected index in player meta for retrieval in callbacks
+			-- FIXME: may not be necessary for buyers
+			pmeta:set_int(ss.modname .. ":selected", 1)
 
-		ss.show_formspec(pos, player)
-	end,
-	can_dig = function(pos, player)
-		return ss.is_shop_owner(pos, player) or ss.is_shop_admin(player)
-	end,
-})
-core.register_alias(ss.modname .. ":shop", ss.modname .. ":sell") -- backward compat
-
-
-core.register_node(ss.modname .. ":buy", {
-	description = S("Buyer Shop"),
-	drawtype = "normal",
-	tiles = {
-		"server_shop_side.png",
-		"server_shop_side.png",
-		"server_shop_side.png",
-		"server_shop_side.png",
-		"server_shop_side.png",
-		"server_shop_front.png",
-		"server_shop_side.png",
+			ss.show_formspec(pos, player, true)
+		end,
 	},
-	groups = {oddly_breakable_by_hand=1,},
-	paramtype2 = "facedir",
-	after_place_node = function(pos, placer)
-		-- set node owner
-		core.get_meta(pos):set_string("owner", placer:get_player_name())
-	end,
-	on_rightclick = function(pos, node, player, itemstack, pointed_thing)
-		local pmeta = player:get_meta()
+}
 
-		-- store node pos in player meta for retrieval in callbacks
-		pmeta:set_string(ss.modname .. ":pos", core.serialize(pos))
-		-- store selected index in player meta for retrieval in callbacks
-		-- FIXME: may not be necessary for buyers
-		pmeta:set_int(ss.modname .. ":selected", 1)
 
-		ss.show_formspec(pos, player, true)
-	end,
-	can_dig = function(pos, player)
-		return ss.is_shop_owner(pos, player) or ss.is_shop_admin(player)
-	end,
-	})
+local nodes = {
+	["sell"] = {def.short, def.sell},
+	["buy"] = {def.short, def.buy},
+	["sell_tall"] = {def.tall, def.sell},
+	["buy_tall"] = {def.tall, def.buy},
+}
+
+for ntype, defs in pairs(nodes) do
+	local full_def = table.copy(def.base)
+	for _, d in ipairs(defs) do
+		for k, v in pairs(d) do
+			full_def[k] = v
+		end
+	end
+
+	core.register_node(ss.modname..":"..ntype, full_def)
 end
